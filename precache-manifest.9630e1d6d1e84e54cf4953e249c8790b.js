c6f2aff5064a1c74332d3768fbb86af4093a9945
scasals808 <54152638+scasals808@users.noreply.github.com>
2020-08-13 23:17:58 +0700

    Updates

---
+++ precache-manifest.9630e1d6d1e84e54cf4953e249c8790b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72bdd14444e815b7c255367f13faebbc",
+    "url": "/TodolistTs/index.html"
+  },
+  {
+    "revision": "ee14e6c62d00a92cf804",
+    "url": "/TodolistTs/static/css/main.541fa914.chunk.css"
+  },
+  {
+    "revision": "6fb6ce4a353de586c353",
+    "url": "/TodolistTs/static/js/2.b2a6acb8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TodolistTs/static/js/2.b2a6acb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee14e6c62d00a92cf804",
+    "url": "/TodolistTs/static/js/main.75785444.chunk.js"
+  },
+  {
+    "revision": "6330a5c123c0d4485a7d",
+    "url": "/TodolistTs/static/js/runtime-main.e89bdd59.js"
+  }
+]);
